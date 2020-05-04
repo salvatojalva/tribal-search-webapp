f5ca294575ed2569c2a97be3717cf43367596066
Salvador Toj <sava.eta@gmail.com>
2020-05-04 03:17:31 -0600

    Update url

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,13 @@ angular.module('tribal', [])
     $scope.search_str = "";
     $scope.media = [];
     $scope.loading_items = [0,1,2,3,4,5,6,7,8]
-    
+    $scope.apiURL = 'http://localhost:8080';
 
     $scope.search = function(){
         $scope.isLoading = true;
         $http({
             method: 'GET',
-            url: 'http://localhost:8080/api/search/'+$scope.search_str
+            url: $scope.apiURL+'/api/search/'+$scope.search_str
         }).then(function successCallback(response) {
             $scope.media = response.data;
         }, function errorCallback(response) {
